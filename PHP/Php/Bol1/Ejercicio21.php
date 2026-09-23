39a2835a3c0bf47c49654c32573d6c89b0f8f26d
mariomm2 <mariomartinmaldonado6@gmail.com>
2026-09-23 12:32:12 +0200

    clase react

--- PHP/Php/Bol1/Ejercicio21.php
+++ PHP/Php/Bol1/Ejercicio21.php
@@ -7,3 +7,35 @@ $productos = [
  ["nombre" => "Gorra", "precio" => 12.0, "stock" => 20],
 ];
 
+
+function productStock0($productos){
+    foreach($productos as $product){
+        if($product["stock"] === 0){
+            echo $product["nombre"] . "<br>";
+        }
+    }
+}
+
+echo productStock0($productos);
+
+function totalInventory($productos){
+    $result = 0;
+    foreach($productos as $product){
+        $result += $product["stock"] * $product["precio"];
+    }
+    return 'Total del stock' ." ". $result;
+}
+
+echo totalInventory($productos);
+
+function orderPrice($productos){
+    usort($productos, function($a,$b){
+        return $b["precio"] - $a["precio"];
+    });
+    foreach($productos as $product){
+        echo $product["nombre"] . " " . $product["precio"] . "<br>";
+    }
+}
+
+echo orderPrice($productos);
+   
